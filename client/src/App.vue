a4e532d8c18a2d6e43423e229be61f7e6c9046b9
Chav Sumalinog <chav@bposeats.com>
2024-01-30 13:08:21 +0800

    Installed axiox; connected frontend to backend

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,44 +14,49 @@
 import TitleHeader from './components/TitleHeader.vue';
 import AddToDo from './components/AddToDo.vue';
 import ToDos from './components/ToDos.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: { TitleHeader, ToDos, AddToDo },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: 'eat',
-          completed: false,
-        },
-        {
-          id: 2,
-          title: 'code',
-          completed: false,
-        },
-        {
-          id: 3,
-          title: 'sleep',
-          completed: false,
-        },
-      ],
+      todos: [],
     };
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter((todo) => todo.id !== id);
+      axios
+        .delete(`http://127.0.0.1:8000/api/todos/${id}`)
+        .then(() => (this.todos = this.todos.filter((todo) => todo.id !== id)))
+        .catch((err) => console.log(err));
     },
-    addTodo(todo) {
-      this.todos = [...this.todos, todo];
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+      axios
+        .post('http://127.0.0.1:8000/api/todos/', { title, completed })
+        .then((res) => (this.todos = [...this.todos, res.data]))
+        .catch((err) => console.log(err));
     },
     completeTodo(todo) {
       const todoItem = this.todos.find((item) => item.id === todo.id);
       if (todoItem) {
-        todoItem.completed = !todoItem.completed;
+        const newCompletedStatus = !todoItem.completed;
+
+        axios
+          .patch(`http://127.0.0.1:8000/api/todos/${todo.id}/`, {
+            completed: newCompletedStatus,
+          })
+          .then((todoItem.completed = newCompletedStatus))
+          .catch((err) => console.log(err));
       }
     },
+  },
+  mounted() {
+    axios
+      .get('http://127.0.0.1:8000/api/todos/')
+      .then((res) => (this.todos = res.data))
+      .catch((err) => console.log(err));
   },
 };
 </script>
--- server/todoloo/todoloo/settings.py
+++ server/todoloo/todoloo/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'todos'
+    'todos',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080'
+]
